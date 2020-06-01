c0e1d6501d54eca0bf05cc2c7292e2e911e8d778
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2020-06-01 05:44:29 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -269,7 +269,7 @@ if (! empty($my_custom_field)) {
 
 						foreach ($author_names as $author_name) {
 
-							echo "$author_name->display_name, &nbsp;";
+							echo "$author_name->display_name<br/>";
 						}
 					}
 						else {echo "No authors found";}
