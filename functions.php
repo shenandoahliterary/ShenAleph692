b19eb090f4aa457bc39f8645f8b9bf56036b3c97
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2020-06-01 05:38:14 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -242,7 +242,7 @@ function shenAleph_filter_authors(){
 	$custom_fields = get_post_custom();
 
 	$my_custom_field = $custom_fields['author_lastname'];
-	echo "$my_custom_field[1]";
+	echo "test2 $my_custom_field[1]";
 
 if (! empty($my_custom_field)) {
 
