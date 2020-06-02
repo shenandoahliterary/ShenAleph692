1210ededff8d549de2f365fac149a87e92535c06
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2020-06-01 21:26:06 -0400

    Update header.php

--- header.php
+++ header.php
@@ -30,17 +30,17 @@
 								<a href="https://shenandoahliterary.org/692/" class="img-fluid-link" rel="home" itemprop="url"><img width="1600" height="228" src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo" srcset="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg 1600w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-300x43.jpg 300w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-768x109.jpg 768w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-1024x146.jpg 1024w" sizes="(max-width: 1600px) 100vw, 1600px" /></a>
 				</div>
 				<div class="row">
-		<div class="col-md-10 offset-md-1">
+		<div class="col offset-md-1">
 			<p id="typelogo"><a href="https://shenandoahLiterary.org/692/">Shenandoah</a></p>
 		</div>
 		</div>
 		<div class="row d-flex justify-content-between">
-			<div class="col-md-5 offset-md-1">
+			<div class="col offset-md-1">
 				<div class="volumeIssueBanner">
 				<span class="">Volume 69, Number 2 &middot; Spring 2020</span>
 				</div>
 		</div>
-			<div class="col-md-5">
+			<div class="col">
 				<div class="shen-nav d-flex flex-column flex-sm-row" >
 					<a class="nav-item nav-link" href="https://shenandoahliterary.org/about/">About</a>
 					<a class="nav-item nav-link" href="https://shenandoahliterary.org/issues/">Issues</a>
